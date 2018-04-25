c11e7d33fd659625fc7c18aff896ae7e42afd11e
Hannah Oberlohr <hannahoberlohr@gmail.com>
2018-04-25 15:05:35 -0500

    Added GalleryDetailModel and Detail view, Included tags in ImageService GetById

--- SimpleImageGallery/Controllers/GalleryController.cs
+++ SimpleImageGallery/Controllers/GalleryController.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using SimpleImageGallery.Models;
-using SimpleImageGallery.Data.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using SimpleImageGallery.Data;
+using SimpleImageGallery.Models;
+using System.Linq;
 
 namespace SimpleImageGallery.Controllers
 {
@@ -26,6 +22,22 @@ namespace SimpleImageGallery.Controllers
                 Images = imageList,
                 SearchQuery = ""
             };
+            return View(model);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var image = _imageService.GetById(id);
+
+            var model = new GalleryDetailModel()
+            {
+                Id = image.Id,
+                Title = image.Title,
+                CreatedOn = image.Created,
+                Url = image.Url,
+                Tags = image.Tags.Select(t => t.Description).ToList()
+            };
+
             return View(model);
         }
     }
